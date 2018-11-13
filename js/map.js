41a5e984e52895b503b8eeeb5b4cbfca651fed60
skomilski.p <skomilski.p@stud.uni-obuda.hu>
2018-11-13 12:32:20 +0200

    Distance calulation

--- js/map.js
+++ js/map.js
@@ -42,3 +42,67 @@ function drawRoutePolyline(routeFile){
 }
 
 
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    var R = 6371e3; // meter
+    var fi1 = deg2rad(lat1);
+    var fi2 = deg2rad(lat2);
+    var deltafi = deg2rad(lat2 - lat1);
+    var deltalambda = deg2rad(lon2 - lon1);
+
+    var a = Math.sin(deltafi / 2) * Math.sin(deltafi / 2) +
+        Math.cos(fi1) * Math.cos(fi2) *
+        Math.sin(deltalambda / 2) * Math.sin(deltalambda / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    var d = R * c;
+    return d; //where d is the disatnce in meter
+}
+
+function deg2rad(degrees) {
+    var pi = Math.PI;
+    return degrees * (pi / 180);
+}
+
+function checkAPIs() {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        // Great success! All the File APIs are supported.
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
+}
+function loadTrajectoryForRacer(racerID, file) {
+
+    function addDoc() {
+        //var file = document.getElementById("myFile").files[0]; //for input type=file
+        var reader = new FileReader();
+        reader.onload = function (e) { }
+        reader.readAsText(file);
+        var error = reader.error;
+        var texte = reader.result;
+        console.log(reader.result);
+        //document.getElementById("DisplayText").innerText = reader.result; /*<p id="DisplayText>*/
+    }
+    /*
+    var textByLine = FileReader.(textfile);
+    for (i = 0; i < textByLine.length; i++) {
+        var tr = textByLine[i].split(" ");
+        var lat = parseFloat(tr[0]);
+        var lng = parseFloat(tr[1]);
+        var position = { lat, lng };
+        routeCoord.push(racerID, lat, lng);
+    }
+*/
+
+    /*var fs = require("fs");
+    fs.readFile(textfile, function (text) {
+        var textByLine = text.split("\n")
+        for (i = 0; i < textByLine.length; i++) {
+            var tr = textByLine[i].split(" ");
+            var lat = parseFloat(tr[0]);
+            var lng = parseFloat(tr[1]);
+            var position = { lat, lng };
+            routeCoord.push(racerID, lat, lng);
+        }
+    });*/
+}
+
