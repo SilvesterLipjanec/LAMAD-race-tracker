9da242e5f443fa1864a2a169cc464fc68bfcd7d6
skomilski.p <skomilski.p@stud.uni-obuda.hu>
2018-11-27 21:02:00 +0200

    Small changes

--- js/visual.js
+++ js/visual.js
@@ -39,9 +39,41 @@ function showFollowedCompInfo(compId,time){
     
 }   
 
-function FitTexts(){  
+//research
+function fitTexts(){  
     var fitties = document.getElementsByClassName(".fit");
     for (let index = 0; index < fitties.length; index++) {
         fitties[index].fit();   
     }
+}
+
+function changeSimulationIndicator(state){
+    var text;
+    switch (state) {
+        case "stop":
+            text="Simulation stopped"
+            break;
+        case "play":
+            text="Simulation in progress..."
+            break;
+        case "pause":
+            text="Simulation paused"
+            break;
+        default:
+            text="";
+            break;
+    }
+    $("#simulationIndicator").text(text);
+}
+
+function rotate180deg(element){
+    var rot = $(element).css('transform');
+    //console.log("before: "+rot);
+    if (rot == "matrix(-1, 1.22465e-16, -1.22465e-16, -1, 0, 0)") {
+        $(element).css({'transform' : 'rotate(0deg)'});
+    } else {
+        $(element).css({'transform' : 'rotate(180deg)'});
+    }
+    rot = $(element).css('transform');
+    //console.log("after: "+rot);
 }
